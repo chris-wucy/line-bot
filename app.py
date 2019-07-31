8119ec44f171f586f540ee7c502666947e49bc82
Chris Wu <christian.wucy@gmail.com>
2019-07-31 19:21:47 +0800

    update

--- app.py
+++ app.py
@@ -44,10 +44,14 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     r = '說人話～！'
-    if msg == 'hi':
+    if msg in ['hi', 'Hi', '嗨']:
         r = '嗨！'
-    elif msg == '你吃飯了嗎？':
+    elif msg == '你吃飯了嗎':
         r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif msg == '訂位':
+        r = '您想訂位，是嗎？'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
