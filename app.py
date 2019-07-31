cdaa2e8e2789fe642e57b29e74c56185b0c535d3
Chris Wu <christian.wucy@gmail.com>
2019-07-31 19:17:34 +0800

    update

--- app.py
+++ app.py
@@ -43,10 +43,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃飯了嗎？'
+    r = '說人話～！'
+    if msg == 'hi':
+        r = '嗨！'
+    elif msg == '你吃飯了嗎？':
+        r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=r))
 
 # 檔案直接被執行才會執行程式
 if __name__ == '__main__':
