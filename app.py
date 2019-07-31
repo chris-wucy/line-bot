89903286ce1896db37263e6efddfeb80369526ee
Chris Wu <christian.wucy@gmail.com>
2019-07-31 19:27:45 +0800

    Sticker

--- app.py
+++ app.py
@@ -50,11 +50,15 @@ def handle_message(event):
         r = '還沒'
     elif msg == '你是誰':
         r = '我是機器人'
-    elif msg == '訂位':
+    elif '訂位' in msg:
         r = '您想訂位，是嗎？'
+    
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+            ))
 
 # 檔案直接被執行才會執行程式
 if __name__ == '__main__':
